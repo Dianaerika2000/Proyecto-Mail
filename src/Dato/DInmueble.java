3f1de7ea09a32f388879939306d52d28dab8769d
Diana Montano <dianaerika.montano16@gmail.com>
2022-07-10 19:25:17 -0400

    package negocio
    CRUD Ninmueble y Dinmueble

--- src/Dato/DInmueble.java
+++ src/Dato/DInmueble.java
@@ -7,7 +7,10 @@ package Dato;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -130,12 +133,18 @@ public class DInmueble {
         PreparedStatement ps = null;
         conexion = new ConexionDB();
         Connection connection = conexion.getConection();
-        String query = "UPDATE inmueble set descripcion = ?, tipo = ?  WHERE codigo = ?";
+        String query = "UPDATE inmueble set descripcion = ?, cod_asignacion = ?, tipo = ?, id_usuario_fk = ?, id_responsable_fk = ?, id_estado_fk = ?, id_direccion_fk = ?, id_grupo_fk = ?  WHERE codigo = ?";
         try {
             ps = connection.prepareStatement(query);
             ps.setString(1, descripcion);
-            ps.setString(2, tipo);
-            ps.setString(3, codigo);
+            ps.setString(2, cod_asignacion);
+            ps.setString(3, tipo);
+            ps.setString(4, id_usuario_fk);
+            ps.setString(5, id_responsable_fk);
+            ps.setInt(6, id_estado_fk);
+            ps.setInt(7, id_direccion_fk);
+            ps.setInt(8, id_grupo_fk);
+            ps.setString(9, codigo);
             ps.executeUpdate();
             conexion.desconectarBD();
             return true;
@@ -160,6 +169,93 @@ public class DInmueble {
             System.out.println("No se elimino " + e);
             return false;
         }
+    }
+    public boolean existe(String codigo){
+        PreparedStatement ps = null;
+        conexion = new ConexionDB();
+        Connection connection = conexion.getConection();
+        ResultSet resultado = null;
+        String query = "SELECT * FROM inmueble WHERE codigo = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, codigo);
+            resultado = ps.executeQuery();
+            if (resultado.next()) {
+                connection.close();
+                return true;
+            } else {
+                connection.close();
+                return false;
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        }
+    }
+    
+    public String listar(String mensaje){
+        conexion = new ConexionDB();
+        String tabla = "";
+        PreparedStatement ps = null;
+        ResultSet resultado = null;
+        tabla = "Content-Type: text/html; charset=\"UTF-8\"\n"
+                + "\n"
+                + "<h3>" + mensaje + "</h3>\n"
+                + "\n"
+                + "<h1>INMUEBLE: </h1>"
+                + "<table style=\"border-collapse: collapse; width: 100%; border: 2px solid black;\">\n"
+                + "\n"
+                + "  <tr>\n"
+                + "\n"
+                + "    <th style = \"text-align: left; padding: 8px; background-color: #c96969; color: white; border: 1px solid red;\">CODIGO</th>\n"
+                + "\n"
+                + "    <th style = \"text-align: left; padding: 8px; background-color: #c96969; color: white; border: 1px solid red;\">DESCRIPCION</th>\n"
+                + "\n"
+                + "    <th style = \"text-align: left; padding: 8px; background-color: #c96969; color: white; border: 1px solid red;\">COD_ASIG</th>\n"
+                + "\n"
+                + "    <th style = \"text-align: left; padding: 8px; background-color: #c96969; color: white; border: 1px solid red;\">TIPO</th>\n"
+                + "\n"
+                + "    <th style = \"text-align: left; padding: 8px; background-color: #c96969; color: white; border: 1px solid red;\">ID_USUARIO</th>\n"
+                + "\n"
+                + "    <th style = \"text-align: left; padding: 8px; background-color: #c96969; color: white; border: 1px solid red;\">ID_RESPONSABLE</th>\n"
+                + "\n"
+                + "    <th style = \"text-align: left; padding: 8px; background-color: #c96969; color: white; border: 1px solid red;\">ID_ESTADO</th>\n"
+                + "\n"
+                + "    <th style = \"text-align: left; padding: 8px; background-color: #c96969; color: white; border: 1px solid red;\">ID_DIRECCION</th>\n"
+                + "\n"
+                + "    <th style = \"text-align: left; padding: 8px; background-color: #c96969; color: white; border: 1px solid red;\">ID_GRUPO</th>\n"
+                + "\n"
+                + "  </tr>\n"
+                + "\n";
+        try {
+            String query = "SELECT * FROM inmueble";
+            Connection connection = conexion.getConection();
+            Statement stmt = connection.createStatement();
+            resultado = stmt.executeQuery(query);
+            ResultSetMetaData rsmd = resultado.getMetaData();
+            int cantCol = rsmd.getColumnCount();
+            while (resultado.next()) {                
+                tabla = tabla
+                        + " <tr>\n"
+                        + "\n";
+                for(int i = 0; i< cantCol; i++){
+                    tabla = tabla = tabla
+                            + "    <td style = \"text-align: left; padding: 8px; border: 1px solid red;\">" + resultado.getString(i + 1) + "</td>\n"
+                            + "\n";
+                }
+                tabla = tabla
+                        + "  </tr>\n"
+                        + "\n";
+            }
+            tabla = tabla
+                    + "\n"
+                    + "</table>";
+            
+            connection.close();
+        } catch (SQLException e) {
+            tabla = "No se puedieron listat los datos";
+        }
+        return tabla;
     }
     
     public static void main(String[] args) {
--- src/Negocio/NInmueble.java
+++ src/Negocio/NInmueble.java
@@ -5,10 +5,152 @@
  */
 package Negocio;
 
+import Dato.DInmueble;
+
 /**
  *
  * @author Vanessa
  */
 public class NInmueble {
+    private DInmueble dInmueble;
+    private String respuesta;
+    
+    public NInmueble(){
+        dInmueble = new DInmueble();
+    }
+    
+    public String registrar(String codigo, String descripcion, String cod_asignacion, String tipo, String id_usuario, String id_responsable, int id_estado, int id_direccion, int id_grupo){
+        if (codigo.length() != 0) {
+            if (descripcion.length() != 0) {
+                if (cod_asignacion.length() != 0) {
+                    if (tipo.length() != 0) {
+                        if (id_usuario.length() != 0) {
+                            if (id_responsable.length() != 0) {
+                                if (Integer.toString(id_estado).length() != 0) {
+                                    if (Integer.toString(id_direccion).length() != 0) {
+                                        if (Integer.toString(id_grupo).length() != 0) {
+                                            dInmueble.setCodigo(codigo);
+                                            dInmueble.setDescripcion(descripcion);
+                                            dInmueble.setCod_asignacion(cod_asignacion);
+                                            dInmueble.setTipo(tipo);
+                                            dInmueble.setId_usuario_fk(id_usuario);
+                                            dInmueble.setId_responsable_fk(id_responsable);
+                                            dInmueble.setId_estado_fk(id_estado);
+                                            dInmueble.setId_direccion_fk(id_direccion);
+                                            dInmueble.setId_grupo_fk(id_grupo);
+                                            if (dInmueble.registrar()) {
+                                                respuesta = dInmueble.listar("Inmueble registrado exitosamente.");
+                                            } else {
+                                                respuesta = "No se logro registrar el inmueble.";
+                                            }
+                                        } else {
+                                            respuesta = "Ingrese un id_grupo que no sea nulo.";
+                                        }
+                                    } else {
+                                        respuesta = "Ingrese un id_direccion que no sea nulo.";
+                                    }
+                                } else {
+                                   respuesta = "Ingrese un id_estado que no sea nulo.";
+                                }
+                            } else {
+                                respuesta = "Ingrese un id_responsable que no sea nulo.";
+                            }
+                        } else {
+                            respuesta = "Ingrese un id_usuario valido.";
+                        }
+                    } else {
+                        respuesta = "Ingrese un tipo que no sea nulo.";
+                    }
+                } else {
+                    respuesta = "Ingrese un codigo de asigancion que no sea nulo.";
+                }
+            } else {
+                respuesta = "Ingrese una descripcion que no sea nula.";
+            }
+        } else {
+            respuesta = "Ingrese un codigo que no sea nulo.";
+        }
+        return respuesta;
+    }
+    
+    public String eliminar(String codigo){
+        if (codigo.length() != 0) {
+            if (dInmueble.existe(codigo)) {
+                dInmueble.setCod_asignacion(codigo);
+                if (dInmueble.eliminar()) {
+                    respuesta = dInmueble.listar("Inmueble elimina exitosamente.");
+                } else {
+                    respuesta = "No se logro eliminar el inmueble.";
+                }
+            } else {
+                respuesta = "El inmueble que quiere eliminar no existe.";
+            }
+        } else {
+            respuesta = "No se permite dato nulo";
+        }
+        return respuesta;
+    }
+    
+    public String listar() {
+        respuesta = dInmueble.listar("");
+        return respuesta;
+    }
+    
+    public String modificar(String codigo, String descripcion, String cod_asignacion, String tipo, String id_usuario, String id_responsable, int id_estado, int id_direccion, int id_grupo){
+        if (codigo.length() != 0) {
+            if (descripcion.length() != 0) {
+                if (cod_asignacion.length() != 0) {
+                    if (tipo.length() != 0) {
+                        if (id_usuario.length() != 0) {
+                            if (id_responsable.length() != 0) {
+                                if (Integer.toString(id_estado).length() != 0) {
+                                    if (Integer.toString(id_direccion).length() != 0) {
+                                        if (Integer.toString(id_grupo).length() != 0) {
+                                            dInmueble.setCodigo(codigo);
+                                            dInmueble.setDescripcion(descripcion);
+                                            dInmueble.setCod_asignacion(cod_asignacion);
+                                            dInmueble.setTipo(tipo);
+                                            dInmueble.setId_usuario_fk(id_usuario);
+                                            dInmueble.setId_responsable_fk(id_responsable);
+                                            dInmueble.setId_estado_fk(id_estado);
+                                            dInmueble.setId_direccion_fk(id_direccion);
+                                            dInmueble.setId_grupo_fk(id_grupo);
+                                            if (dInmueble.modificar()) {
+                                                respuesta = dInmueble.listar("Inmueble modificado exitosamente.");
+                                            } else {
+                                                respuesta = "No se logro modificar el inmueble.";
+                                            }
+                                        } else {
+                                            respuesta = "Ingrese un id_grupo que no sea nulo.";
+                                        }
+                                    } else {
+                                        respuesta = "Ingrese un id_direccion que no sea nulo.";
+                                    }
+                                } else {
+                                   respuesta = "Ingrese un id_estado que no sea nulo.";
+                                }
+                            } else {
+                                respuesta = "Ingrese un id_responsable que no sea nulo.";
+                            }
+                        } else {
+                            respuesta = "Ingrese un id_usuario valido.";
+                        }
+                    } else {
+                        respuesta = "Ingrese un tipo que no sea nulo.";
+                    }
+                } else {
+                    respuesta = "Ingrese un codigo de asigancion que no sea nulo.";
+                }
+            } else {
+                respuesta = "Ingrese una descripcion que no sea nula.";
+            }
+        } else {
+            respuesta = "Ingrese un codigo que no sea nulo.";
+        }
+        return respuesta;
+    }
+    
+     
+    
     
 }
